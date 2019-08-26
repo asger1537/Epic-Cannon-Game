7ab445fb530bded68e126982b92f51821e78be21
asger1537 <34832830+asger1537@users.noreply.github.com>
2019-08-26 08:50:30 +0200

    Added Cannon class method descriptions

---
+++ CannonGame/src/CannonGame/Cannon.java
@@ -0,0 +1,41 @@
+package CannonGame;
+
+import processing.core.PVector;
+
+public class Cannon {
+	PVector position;
+	
+	float angleVelocity;
+	float angle;
+	float movementSpeed;
+	float barrelLength;
+	
+	Cannon(PVector position){
+		this.position = position;
+		this.angle = 0f;
+		this.angleVelocity = 0.1f;
+		this.movementSpeed = 5f;
+		this.barrelLength = 50f;
+	}
+	
+	void update() {
+		move();
+		show();
+		
+	}
+	
+	//moving the cannon and barrel
+	void move() {
+		
+	}
+	
+	//drawing the cannon - i.e the barrel, wheels and maybe some extra
+	void show() {
+		
+	}
+	
+	void shoot() {
+	
+	}
+	
+}
---
+++ CannonGame/src/CannonGame/CannonGame.java
@@ -0,0 +1,25 @@
+package CannonGame;
+
+import processing.core.PApplet;
+
+public class CannonGame extends PApplet{
+	public static CannonGame applet;
+	
+	//only the window size needs to be set here
+	public void settings() {
+		//for testing purposes the size can be used, but otherwise use fullscreen
+		size(800, 800);
+		//fullScreen();
+	}
+	
+	//initialize things
+	public void setup() {
+		
+		
+	}
+	
+	//called every frame
+	public void draw() {
+		
+	}
+}
