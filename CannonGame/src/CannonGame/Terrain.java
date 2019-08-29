d2aa4ee729af085554e93a20cb37d2856e0e878b
ulriku <ulrikhlp@gmail.com>
2019-08-29 16:10:13 +0200

    Create Terrain.java
    #3 start på kode
    #4  start på kode

---
+++ CannonGame/src/CannonGame/Terrain.java
@@ -0,0 +1,22 @@
+package CannonGame;
+
+import processing.core.PVector;
+
+
+public class Terrain{
+	int width;
+	ArrayList<int> heights;
+
+Terrain(int width){
+	this.width = width;
+	heights = new ArrayList<int>(width);
+}	
+
+void generate(){
+//todo
+}
+
+boolean checkCollision(SquareBall squareball)
+	return false;
+//todo 
+}
