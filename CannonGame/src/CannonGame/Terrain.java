de9121fe7b2c9f7c7312e520ba1a31858b4f1556
ulriku <ulrikhlp@gmail.com>
2019-09-02 21:05:35 +0200

    work on terrain generation
    #4 midpoint displacement algorithm for terrain generation //todo display and finish terrain generation

--- CannonGame/src/CannonGame/Terrain.java
+++ CannonGame/src/CannonGame/Terrain.java
@@ -1,19 +1,40 @@
 package CannonGame;
-
+import java.util.LinkedList; 
+import java.util.Queue; 
 import processing.core.PVector;
 
 
 public class Terrain{
-	int width;
-	ArrayList<int> heights;
+int height = height;
+int width = width;
+int offset = height/4; 
 
 Terrain(int width){
 	this.width = width;
-	heights = new ArrayList<int>(width);
+	this.height = height;
+	heightmap = new ArrayList<int>(width);
 }	
 
-void generate(){
-//todo
+void generate()
+{
+Queue<Integer> q = new LinkedList<>();
+
+q.add(0);
+q.add(width-1);
+while(!q.isEmpty())
+	{
+		int x0 = q.remove();
+		int x1 = q.remove();
+		int mid = (x0 + x1)/2;
+		heightmap[mid] = (heightmap[x0] + heightmap[x1])/2+random(-offset,offset);
+		if(x1-mid > 1)
+		{
+			q.add(x0);
+			q.add(mid);
+			q.add(mid);
+			q.add(x1);
+		}
+	}
 }
 
 boolean checkCollision(SquareBall squareball)
