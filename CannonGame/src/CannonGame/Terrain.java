b8a89c190b9663f37488174bcb83e866d518e2ce
ulriku <ulrikhlp@gmail.com>
2019-09-03 14:35:23 +0200

    minor bug fix and roughenss added
    #4 added roughness for a more smooth curve display and generation works well.

--- CannonGame/src/CannonGame/Terrain.java
+++ CannonGame/src/CannonGame/Terrain.java
@@ -19,37 +19,42 @@ Terrain()
 
 void generate()
 {
-	int[] heightmap = new int[tWidth]; 
+	heightmap = new int[tWidth]; 
 	// left terrain point 
 	heightmap[0]= (int)applet.random(tHeight);
 	// right terrain point 
 	heightmap[tWidth-1]= (int)applet.random(tHeight);
 	// make a queue
-	Queue<Integer> q = new LinkedList<>();
+	Queue<Integer> q = new LinkedList<Integer>();
 
 // add first segment (left and right x terrain point)	
 q.add(0);
 q.add(tWidth-1);
+q.add(offset);
 //generates terrain until distance between points is not divisable by 2
 while(!q.isEmpty())
 	{
-		// set x0 to first in queue and remove from queue
+		// set x0 value to that which was first in queue and remove that which it was set to from the queue (0)
 		int x0 = q.remove();
-		// set x1 to first in queue and remove it from the queue
+		// set x1 value to that which was first in queue and remove that which it was set to from the queue (width-1)
 		int x1 = q.remove();
 		// the x-coord for mid point is avearge of the two points
 		int mid = (x0 + x1)/2;
+		// set roughness value to that which was first in queue and remove that which it was set to from the queue (offset)
+		int roughness = q.remove();
 		// the y-coord for mid point is avearge of left and right  y-coord point + a random offset
-		heightmap[mid] =(int)((heightmap[x0]+heightmap[x1])/2+applet.random(-offset,offset));
+		heightmap[mid] =(int)((heightmap[x0]+heightmap[x1])/2+applet.random(-roughness,roughness));
 		//continue if divisable by 2
 		if(x1-mid > 1)
 		{
 			// add the left segment (made up of the middle point as the "new" right point)
 			q.add(x0);
 			q.add(mid);
+			q.add(roughness/2); // divide roughness for a more smooth curve
 			// add the right segment (made up of the middle point as the "new" left point)
 			q.add(mid);
 			q.add(x1);
+			q.add(roughness/2); // divide roughness for a more smooth curve
 		}
 	}
 }
@@ -57,7 +62,7 @@ while(!q.isEmpty())
 void displayTerrain()
 {
 	//loop through length of array
-for(int i = 0; i < tWidth-1; i++)
+for(int i = 0; i < heightmap.length-1; i++)
 	{
 	//the vertical line from the botoom of the screen to the current terrain point
 	applet.line(i,applet.height,i,heightmap[i]);
