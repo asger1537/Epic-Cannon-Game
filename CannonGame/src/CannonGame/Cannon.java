5f655e5ccbcf78242f7cc9de95b3b7c336049a7f
ActualNigerianPrince <54433929+ActualNigerianPrince@users.noreply.github.com>
2019-09-03 10:37:09 +0200

    updated show function for cannon
    #7

--- CannonGame/src/CannonGame/Cannon.java
+++ CannonGame/src/CannonGame/Cannon.java
@@ -1,6 +1,8 @@
 package CannonGame;
 
+import processing.core.PConstants;
 import processing.core.PVector;
+import static CannonGame.CannonGame.applet;
 
 
 public class Cannon {
@@ -10,13 +12,20 @@ public class Cannon {
 	float angle;
 	float movementSpeed;
 	float barrelLength;
+	float barrelWidth;
+	float wheelRadius;
+	float wheelCount;
+	float a;
 	
 	Cannon(PVector position){
+		this.wheelCount = 5;
+		this.wheelRadius = 30f;
+		this.barrelWidth = 20f;
 		this.position = position;
-		this.angle = 0f;
+		this.angle = -PConstants.PI;
 		this.angleVelocity = 0.1f;
 		this.movementSpeed = 5f;
-		this.barrelLength = 50f;
+		this.barrelLength = 100f;
 	}
 	
 	void update() {
@@ -31,7 +40,36 @@ public class Cannon {
 	
 	//drawing the cannon - i.e the barrel, wheels and maybe some extra
 	void show() {
-		
+		a++;
+		applet.pushMatrix();
+  applet.translate(position.x,position.y-30);  
+  //applet.rotate(angle/((180)/a));
+  applet.pushMatrix();
+  applet.rotate(angle/(180/a));
+  applet.rectMode(3);
+  applet.rect(barrelLength/2, 0, barrelLength, barrelWidth);
+  applet.popMatrix();
+  
+  for(int i=0; i<wheelCount; i++) {
+	applet.ellipse(wheelRadius*2*i-4*wheelRadius, 55+wheelRadius, wheelRadius*2, wheelRadius*2);  
+	}
+	applet.beginShape();
+	applet.vertex(-5*wheelRadius,55+wheelRadius);
+	applet.vertex(5*wheelRadius,55+wheelRadius);
+	applet.vertex(150,55);
+	applet.vertex(100,5);
+	applet.vertex(50,5);
+	applet.vertex(50,-15);
+	applet.vertex(-50,-15);
+	applet.vertex(-50,5);
+
+	applet.vertex(-100,5);
+	applet.vertex(-150,55);
+	applet.endShape(2);
+  applet.rect(0,-10,100,30);
+  
+ // println((atan(barrel.x/barrel.y))/PI*180);
+  applet.popMatrix();
 	}
 	
 	void shoot() {
